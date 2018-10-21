88ef804afe61c4250862b7f92a31f0997bfc7c75
Troy B. Jones <tbjones@draper.com>
2018-10-21 08:38:14 -0400

    now use only March 10 virtualagc commit for new images

--- stage4/69-dsky/00-run.sh
+++ stage4/69-dsky/00-run.sh
@@ -3,6 +3,14 @@
 
 git -C "${ROOTFS_DIR}/home/pi" clone https://github.com/virtualagc/virtualagc.git 
 
+# as of 10/20/18, just building from master is broken on DSKY
+# use March 10 for building unti further notice - TBJ
+
+# now checkout the March 10 commit - rollsback the repo to this date
+# https://github.com/virtualagc/virtualagc/commit/4abd659b9bc2fb062a6cb1da3798071d8d75d962
+
+git checkout 4abd659
+
 # make all files owned by pi user
 chown 1000:1000 "${ROOTFS_DIR}/home/pi/virtualagc" -Rv
 
