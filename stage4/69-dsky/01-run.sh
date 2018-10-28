6da2e14c038aaabf9e23f2741562ed11186fcc44
Troy B. Jones <tbjones@draper.com>
2018-10-28 08:08:47 -0400

    added default xorg touch calibration file

--- stage4/69-dsky/01-run.sh
+++ stage4/69-dsky/01-run.sh
@@ -2,4 +2,7 @@
 # extract the waveshare driver build scripts
 tar xvf files/LCD-show-180331.tar.gz -C "${ROOTFS_DIR}/home/pi"
 
+# copy the default screen calibraiton file to xorg.conf.d
+# WARNING, expect some small differences between screens
+install -v -m 644 files/99-calibration.conf	"${ROOTFS_DIR}/usr/share/X11/xorg.conf.d/"
 
