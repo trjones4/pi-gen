676deaad6441002b5205d7dcf93a69747b746700
Troy B. Jones <tbjones@draper.com>
2018-10-28 08:55:31 -0400

    patched python to fix seperators - BAD!

---
+++ stage4/69-dsky/03-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+# REMOVE ME AS SOON AS POSSIBLE!!
+# patch the python piDSKY2.py to fix seperators
+install -v -o 1000 -g 1000 -m 644 "files/piDSKY2.py" "${ROOTFS_DIR}/home/pi/virtualagc/piPeripheral/"
+
