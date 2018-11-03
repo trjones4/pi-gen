203f28182cd77ef10cda05d5ff4efb63164f628f
Troy B. Jones <tbjones@draper.com>
2018-11-03 15:27:18 -0400

    made pyDSKY2 +x for running

--- stage4/69-dsky/03-run.sh
+++ stage4/69-dsky/03-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 # REMOVE ME AS SOON AS POSSIBLE!!
 # patch the python piDSKY2.py to fix seperators
-install -v -o 1000 -g 1000 -m 644 "files/piDSKY2.py" "${ROOTFS_DIR}/home/pi/virtualagc/piPeripheral/"
+install -v -o 1000 -g 1000 -m 755 "files/piDSKY2.py" "${ROOTFS_DIR}/home/pi/virtualagc/piPeripheral/"
 
